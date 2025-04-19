dc07fd2fb541cfe4f822da62964e2df9269959b3
Никита Бондаренко <nikbondik00@gmail.com>
2025-04-19 11:08:14 +0300

    Добавлен раздел "Мои курсы"

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,20 +1,70 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using BLL_Education.DTO;
+using BLL_Education.Profiles;
+using BLL_Education.Services;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace EduPlatform.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly CourseService courseService;
+        private readonly EnrollmentService enrollmentService;
+
+        public StudentController()
+        {
+            var mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new CourseProfile());
+                cfg.AddProfile(new EnrollmentProfile());
+            }).CreateMapper();
+
+            courseService = new CourseService(mapper);
+            enrollmentService = new EnrollmentService(mapper);
+        }
+
         // GET: StudentController
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: StudentController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult MyCourses()
         {
-            return View();
+
+            var studentId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            try
+            {
+                var studentEnrollments = enrollmentService.GetAllForStudent(studentId);
+
+                if (studentEnrollments == null || !studentEnrollments.Any())
+                {
+                    return View(new List<CourseDTO>());
+                }
+
+                var studentCourses = new List<CourseDTO>();
+                foreach (var enrollment in studentEnrollments)
+                {
+                    if (enrollment.CourseId > 0)
+                    {
+                        var course = courseService.FindById(enrollment.CourseId);
+                        if (course != null)
+                        {
+                            studentCourses.Add(course);
+                        }
+                    }
+                }
+
+                return View(studentCourses);
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
 
         // GET: StudentController/Create
